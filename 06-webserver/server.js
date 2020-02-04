991a65eb0b4a2b46890a0b5feeb79b46851805ef
Kevin Tellez <ktellez.teamdevco@gmail.com>
2020-02-04 13:59:23 -0500

    iframe test

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -11,7 +11,11 @@ app.use(express.static( __dirname + '/public'));
 hbs.registerPartials( __dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://kdts-node-course.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', (req, res) => {
     let data = {
